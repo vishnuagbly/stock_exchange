a7474556101c464d19cff2f0532ff1cc3e7abc7c
ViLSa <vishnuagbly@gmail.com>
2020-09-22 17:03:25 +0530

    added screenshots

--- android/app/src/main/java/com/vishnuworld/stockexchange/MainActivity.java
+++ android/app/src/main/java/com/vishnuworld/stockexchange/MainActivity.java
@@ -2,19 +2,5 @@ package com.vishnuworld.stockexchange;
 
 import io.flutter.embedding.android.FlutterActivity;
 
-package org.devio.flutter.splashscreen.example;
-import android.os.Bundle;
-
-+ import org.devio.flutter.splashscreen.flutter_splash_screen.SplashScreen;
-
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
 public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        +       SplashScreen.show(this, true);// here
-        super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(this);
-    }
 }
